cf0fa0c6b89f3118778cfdcee7ce768d934b40c5
Maschell <Maschell@gmx.de>
2017-08-30 17:58:45 +0200

    Added the missing defines and add some new one

--- src/common/common.h
+++ src/common/common.h
@@ -1,5 +1,5 @@
 #ifndef COMMON_H
-#define	COMMON_H
+#define    COMMON_H
 
 #ifdef __cplusplus
 extern "C" {
@@ -8,7 +8,7 @@ extern "C" {
 #include "os_defs.h"
 
 #define CAFE_OS_SD_PATH             "/vol/external01"
-#define GAME_MOD_FOLDER		        "/sdcafiine"
+#define GAME_MOD_FOLDER             "/sdcafiine"
 #define SD_PATH                     "sd:"
 #define USB_PATH                    "usb:"
 #define DEFAULT_NAME_PACKAGE        "DEFAULT"
@@ -16,19 +16,28 @@ extern "C" {
 #define NAME_PREFIX_USB             "usb:"
 #define WIIU_PATH                   "/wiiu"
 #define IP_TXT                      "ip.txt"
-#define VERSION						"1.4a (experimental)"
+#define VERSION                     "1.4b (experimental)"
 
-#define CONTENT_FOLDER				"content"
-#define AOC_FOLDER				    "aoc"
+#define CONTENT_FOLDER              "content"
+#define AOC_FOLDER                  "aoc"
+#define META_FOLDER                 "meta"
+
+#define BOOT_TV_TEX_TGA             "bootTvTex.tga"
+#define BOOT_DRC_TEX_TGA            "bootDrcTex.tga"
+#define BOOT_SOUND_BTSND            "bootSound.btsnd"
 
 #define GAME_PATH_TYPE_CONTENT      1
 #define GAME_PATH_TYPE_AOC          2
 
 #define USE_OS_FS_FUNCTION          -1337
 
+#define SDUSB_MOUNTED_NONE          0
+#define SDUSB_MOUNTED_FAKE          (1<<0)
+#define SDUSB_MOUNTED_OS_SD         (1<<1)
+#define SDUSB_MOUNTED_LIBIOSUHAX    (1<<2)
 
-#define FILELIST_NAME			    "filelist.txt"
-#define DIR_IDENTIFY			    "?"  /* maximum length = 1*/
+#define FILELIST_NAME               "filelist.txt"
+#define DIR_IDENTIFY                "?"  /* maximum length = 1*/
 #define PARENT_DIR_IDENTIFY         "?.."
 
 /* Macros for libs */
@@ -57,7 +66,7 @@ extern "C" {
 #define LIB_LOADER              0x1001
 
 #ifndef MEM_BASE
-#define MEM_BASE                    (0x00800000)
+#define MEM_BASE                (0x00800000)
 #endif
 
 #define ELF_DATA_ADDR               (*(volatile unsigned int*)(MEM_BASE + 0x1300 + 0x00))
@@ -77,5 +86,5 @@ extern "C" {
 }
 #endif
 
-#endif	/* COMMON_H */
+#endif    /* COMMON_H */
 
