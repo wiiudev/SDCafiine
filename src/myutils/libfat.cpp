538d3650b88074cb34888e91f9fa905f192c40d3
Maschell <Maschell@gmx.de>
2018-09-21 17:21:47 +0200

    Fix caching value for libfat.

--- src/myutils/libfat.cpp
+++ src/myutils/libfat.cpp
@@ -4,17 +4,19 @@
 #include <fat.h>
 #include "common/retain_vars.h"
 
-int mount_libfatAll(){
-    int res = -1;
-    if((res = fatInitDefault()) >= 0){
+#define LIBFAT_WIIU_DEFAULT_CACHE_PAGES 4
+#define LIBFAT_WIIU_DEFAULT_SECTORS_PAGE 64
+
+int32_t mount_libfatAll() {
+    int32_t res = -1;
+    if((res = fatInitEx (LIBFAT_WIIU_DEFAULT_CACHE_PAGES, true, LIBFAT_WIIU_DEFAULT_SECTORS_PAGE)) >= 0) {
         DEBUG_FUNCTION_LINE("fatInitDefault success\n");
         return 0;
-    }else{
+    } else {
         DEBUG_FUNCTION_LINE("fatInitDefault failed %d\n",res);
     }
     return -1;
 }
-
-void unmount_libfat(const char * path){
+void unmount_libfat(const char * path) {
     fatUnmount(path);
 }
